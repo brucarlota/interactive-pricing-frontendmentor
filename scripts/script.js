10cb9d4eca03fb6dcaf5896254685d45ccd57913
bruna carlota <bru.carlota@yahoo.com.br>
2021-12-26 22:08:56 -0300

    pageviews update

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,11 @@
-let values = [8, 12, 16, 24, 36];
+const values = [8, 12, 16, 24, 36];
+const views = ['10K', '50K', '100K', '500K', '1M'];
 
 let rangeInput = document.getElementById('rangeSlider');
 let priceOutput = document.querySelector('.price');
+const viewsOutput = document.querySelector('.pageviews');
 
 rangeInput.addEventListener('input', function() {
   priceOutput.innerHTML = `$${values[rangeInput.value]}.00 <span>/ month</span>`;
+  viewsOutput.innerHTML = `${views[rangeInput.value]} pageviews`;
 });
