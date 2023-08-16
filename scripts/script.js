a8da44e472be194f16b18d7631d6e188cafd8f2b
brucarlota <bru.carlota@yahoo.com.br>
2023-08-16 08:27:19 -0300

    Fix input range

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,9 @@ let priceOutput = document.querySelector('.price');
 const viewsOutput = document.querySelector('.pageviews');
 
 rangeInput.addEventListener('input', function() {
+  const value = this.value * 25;
   priceOutput.innerHTML = `$${values[rangeInput.value]}.00 <span>/ month</span>`;
   viewsOutput.innerHTML = `${views[rangeInput.value]} pageviews`;
+
+  this.style.background = `linear-gradient(to right, #82CFD0 0%, #82CFD0 ${value}%, #ECF0FB ${value}%, #ECF0FB 100%)`;
 });
